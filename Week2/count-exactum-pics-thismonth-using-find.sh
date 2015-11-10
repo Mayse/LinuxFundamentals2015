9a8f488d7caf491f8b8f863045aa1d6835161a88
Mayse <mayse@iki.fi>
2015-11-10 10:15:41 +0200

    count-exactum-pics-thismonth-using-find.sh uses -file "*.jpg" option

--- Week2/count-exactum-pics-thismonth-using-find.sh
+++ Week2/count-exactum-pics-thismonth-using-find.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 year=`date +%Y`
 month=`date +%m`
-find /home/tktl-csfs/home/tkt_cam/public_html/$year/$month -type f| grep '\.jpg$' | wc -l
+find /home/tktl-csfs/home/tkt_cam/public_html/$year/$month -type f -name "*.jpg"| grep '\.jpg$' | wc -l
